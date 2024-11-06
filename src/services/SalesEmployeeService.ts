73f4033b51afa009fc31630c5e8040545bf7927b
Dan Adams <daniel.adams@kainos.com>
2024-11-06 15:49:32 +0000

    ILE-3 got rid of plusses

--- src/services/SalesEmployeeService.ts
+++ src/services/SalesEmployeeService.ts
@@ -4,7 +4,7 @@ import { SalesEmployeeRequest } from "../models/SalesEmployeeRequest";
 
 export const getSalesEmployees = async (): Promise<SalesEmployeeResponse[]> => {
     try {
-        const response: AxiosResponse = await axios.get(process.env.API_URL + "/sales-employee");
+        const response: AxiosResponse = await axios.get(`${process.env.API_URL}/sales-employee`);
 
         return response.data;
     } catch (e) {
@@ -14,7 +14,7 @@ export const getSalesEmployees = async (): Promise<SalesEmployeeResponse[]> => {
 
 export const getSalesEmployee = async (id: String): Promise<SalesEmployeeResponse[]> => {
     try {
-        const response: AxiosResponse = await axios.get(process.env.API_URL + "/sales-employee/" + id);
+        const response: AxiosResponse = await axios.get(`${process.env.API_URL}/sales-employee/${id}`);
 
         return response.data;
     } catch (e) {
@@ -24,7 +24,7 @@ export const getSalesEmployee = async (id: String): Promise<SalesEmployeeRespons
 
 export const deleteSalesEmployee = async (id: String): Promise<void> => {
     try {
-        const response: AxiosResponse = await axios.delete(process.env.API_URL + "/sales-employee/" + id);
+        const response: AxiosResponse = await axios.delete(`${process.env.API_URL}/sales-employee/${id}`);
 
         return response.data;
     } catch (e) {
@@ -41,7 +41,7 @@ export const createSalesEmployee = async (salesEmployee: SalesEmployeeRequest): 
             commissionRate: salesEmployee.commissionRatePercentage / 100
         };
 
-        const response: AxiosResponse = await axios.post(process.env.API_URL + "/sales-employee", salesEmployeeRequest);
+        const response: AxiosResponse = await axios.post(`${process.env.API_URL}/sales-employee`, salesEmployeeRequest);
 
         return response.data;
     } catch (e) {
@@ -58,7 +58,7 @@ export const editSalesEmployee = async (salesEmployee: SalesEmployeeRequest, id:
             commissionRate: salesEmployee.commissionRatePercentage / 100
         };
 
-        const response: AxiosResponse = await axios.put(process.env.API_URL + "/sales-employee/" + id, salesEmployeeRequest);
+        const response: AxiosResponse = await axios.put(`${process.env.API_URL}/sales-employee/${id}`, salesEmployeeRequest);
 
         return response.data;
     } catch (e) {
