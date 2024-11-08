480c8fb85eefc420f747147e81d92fb03be5c6f6
dylanmckk <dylan.mckenzie@kainos.com>
2024-11-08 10:08:03 +0000

    ILE-3 Client Backend

--- src/middleware/CheckClientExistsMiddleware.ts
+++ src/middleware/CheckClientExistsMiddleware.ts
@@ -4,7 +4,7 @@ import { getClient } from "../services/ClientService";
 export const checkClientExists = () => {
     return async (req: express.Request, res: express.Response, next: express.NextFunction) => {
         try {
-            const client = await getClient(req.params.id);
+            await getClient(req.params.id);
         } catch (e) {
             throw new Error('Failed to delete client');
         }
